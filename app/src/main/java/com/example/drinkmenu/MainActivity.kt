45ecc1efa39fe47f4b6b088515d20b30eb4487a1
noranmohamed90 <nm272319@gmail.com>
2025-12-02 20:58:54 +0200

    second

--- app/src/main/java/com/example/drinkmenu/MainActivity.kt
+++ app/src/main/java/com/example/drinkmenu/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.drinkmenu
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,7 +12,9 @@ import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,13 +34,19 @@ class MainActivity : AppCompatActivity() {
     lateinit var price :TextView
     lateinit var orderBtn :Button
 
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeViews()
         menuAdapter()
+        dropDownMenu.setOnItemClickListener { parent, view, position, id ->
+            selectedDrink()
+        }
+        orderSubmitted()
     }
+
 
     private fun initializeViews(){
         menuET =findViewById(R.id.orderTF)
@@ -49,5 +59,31 @@ class MainActivity : AppCompatActivity() {
         val listOfJuice = listOf(orangeJuice,mangoJuice,appleJuice,kiwiJuice)
         val menuAdapter =ArrayAdapter(this,R.layout.drop_down_menu,listOfJuice)
         dropDownMenu.setAdapter(menuAdapter)
+    }
+    private fun selectedDrink(){
+        val selecteddDrink = dropDownMenu.text.toString()
+        var actualPrice = juicePrice[dropDownMenu.text.toString()]
+            if (selecteddDrink == orangeJuice) {
+                price.setText(actualPrice.toString())
+            } else if (selecteddDrink == mangoJuice) {
+                price.setText(actualPrice.toString())
+            } else if (selecteddDrink == appleJuice) {
+                price.setText(actualPrice.toString())
+            } else if (selecteddDrink == kiwiJuice) {
+                price.setText(actualPrice.toString())
+            }
+    }
+    private fun orderSubmitted(){
+        orderBtn.setOnClickListener {
+            val selecteddDrink = dropDownMenu.text.toString()
+            val orderIntent = Intent(Intent.ACTION_SENDTO)
+            orderIntent.data= Uri.parse("mailto: noranmoahmed90@gmail.com?subject=Drink Menu&body=I want to order $selecteddDrink")
+            orderIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("nm272319@gmail.com"))
+            try {
+                startActivity(orderIntent)
+            }catch (e:Exception){
+                Toast.makeText(this, "No email app found", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
