1eab43629d55b92030b3ef089c866e1d7268b3ea
noranmohamed90 <nm272319@gmail.com>
2025-12-04 01:20:49 +0200

    Last

--- app/src/main/java/com/example/drinkmenu/MainActivity.kt
+++ app/src/main/java/com/example/drinkmenu/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         kiwiJuice to 45,
         appleJuice to 25
     )
-    lateinit var menuET :TextInputLayout
+
     lateinit var dropDownMenu :AutoCompleteTextView
     lateinit var price :TextView
     lateinit var orderBtn :Button
@@ -42,42 +42,30 @@ class MainActivity : AppCompatActivity() {
         initializeViews()
         menuAdapter()
         dropDownMenu.setOnItemClickListener { parent, view, position, id ->
-            selectedDrink()
+            selectedItem()
         }
         orderSubmitted()
     }
-
-
     private fun initializeViews(){
-        menuET =findViewById(R.id.orderTF)
         dropDownMenu = findViewById(R.id.menuComplete)
         price = findViewById(R.id.priceTV)
         orderBtn = findViewById(R.id.submittedBtn)
-
     }
     private fun menuAdapter(){
         val listOfJuice = listOf(orangeJuice,mangoJuice,appleJuice,kiwiJuice)
         val menuAdapter =ArrayAdapter(this,R.layout.drop_down_menu,listOfJuice)
         dropDownMenu.setAdapter(menuAdapter)
     }
-    private fun selectedDrink(){
-        val selecteddDrink = dropDownMenu.text.toString()
-        var actualPrice = juicePrice[dropDownMenu.text.toString()]
-            if (selecteddDrink == orangeJuice) {
-                price.setText(actualPrice.toString())
-            } else if (selecteddDrink == mangoJuice) {
-                price.setText(actualPrice.toString())
-            } else if (selecteddDrink == appleJuice) {
-                price.setText(actualPrice.toString())
-            } else if (selecteddDrink == kiwiJuice) {
-                price.setText(actualPrice.toString())
-            }
+    private fun selectedItem(){
+        val selectedDrink = dropDownMenu.text.toString()
+        val selectedPriceDrink = juicePrice[selectedDrink]
+        price.setText(selectedPriceDrink.toString())
     }
     private fun orderSubmitted(){
         orderBtn.setOnClickListener {
-            val selecteddDrink = dropDownMenu.text.toString()
             val orderIntent = Intent(Intent.ACTION_SENDTO)
-            orderIntent.data= Uri.parse("mailto: noranmoahmed90@gmail.com?subject=Drink Menu&body=I want to order $selecteddDrink")
+            orderIntent.data= Uri.parse("mailto: noranmoahmed90@gmail.com?subject=Drink Menu&body=I want to order ${dropDownMenu.text.toString()}" +
+                    "!")
             orderIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("nm272319@gmail.com"))
             try {
                 startActivity(orderIntent)
